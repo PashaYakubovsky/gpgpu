697ab7012823b3de2e14b20ba80989207f8d28b4
PashaDev2 <pasha.yakubovsky@lynksen.com>
2024-10-15 03:20:09 +0300

    +fix build 2

--- src/scene.ts
+++ src/scene.ts
@@ -1,6 +1,7 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import Stats from "three/examples/jsm/libs/stats.module";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
+import Stats from "three/examples/jsm/libs/stats.module.js";
+
 import fragmentShader from "./shaders/fragment.glsl";
 import vertexShader from "./shaders/vertex.glsl";
 import simFragmentShader from "./shaders/simFragment.glsl";
